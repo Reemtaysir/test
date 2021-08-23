9f042d8165c982b6050bc8bdf46f5ab0e9121de5
Rami Abboud <rami.abboud.@tatweer.sy>
2021-08-23 12:01:09 +0300

    rami

---
+++ src/main/java/com/ensat/controllers/IndexController.java
@@ -0,0 +1,22 @@
+package com.ensat.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * Homepage controller.
+ */
+@Controller
+public class IndexController {
+
+    @RequestMapping("/")
+    String index() {
+        return "index";
+    }
+
+    @RequestMapping("/")
+    String index3() {
+        return "index";
+    }
+
+}
